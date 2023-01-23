c3f8796cb92a68f95707d3a3fd3da608670cf750
Keegan G <keegangallagher2006@gmail.com>
2023-01-23 14:42:56 -0500

    print Statements

---
+++ TheDeclarationOfIndependance.py
@@ -0,0 +1,16 @@
+# Programmer: Keegan Gallagher
+# Date: 01.23.23
+# Program: InfoTech Center Upgrades
+
+"""
+Our Welcome Screen will start our Program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+
+
+
+
+
+print("\n\nWelcome - InfoTechCenter 4.0")
+print("\ninFoTech Center is loading")
