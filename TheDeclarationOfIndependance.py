ee61196e8272055ed961d67dc03c0d1e112ad379
Keegan G <keegangallagher2006@gmail.com>
2023-01-30 13:28:09 -0500

    Adding Color and Changing txt/Spaces
    Stable

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -15,19 +15,20 @@ timeToSleep = 2
 i = "."
 x = 1
 
-print("\n\nWelcome - InfoTechCenter 4.0")
+print("\n\n\033[1;35;40mWelcome - InfoTechCenter 4.0")
 time.sleep(timeToSleep)
 #print (InfoCenter 4.0 is loading
 x = 0
-a =0
+a = 0
 
 while x != 20:
     x += 1
-    b = ("\033[1;33;40m InfoCeter 4.0 is loading" + "." * a)
+    b = ("\033[1;33;40mInfoCeter 4.0 is loading" + "." * a)
     a = a + 1
     sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\n\033[1;32;40mMission Accomplished!')
+done = 'false'
